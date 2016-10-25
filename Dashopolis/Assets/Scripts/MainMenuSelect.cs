ae16dd3af621a4220bebf6b7a0f2c7003faa6a33
Brendan <brendan.blencowe@gmail.com>
2016-10-25 19:17:53 -0400

    Added MainMenu scene

---
+++ Dashopolis/Assets/Scripts/MainMenuSelect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSelect : MonoBehaviour {
+
+	public void loadCharacterSelect(){
+		SceneManager.LoadScene ("Cave");
+	}
+}
