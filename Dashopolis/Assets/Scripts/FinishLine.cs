c270912470eca8b874f639e9aad29fc5b88e120a
chenandy-ca <chenandy92@hotmail.com>
2016-10-27 14:44:00 -0400

    trying to fix the scene
    it might break yolo

---
+++ Dashopolis/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	
+	void OnCollisionExit2D(Collision2D col){
+		//if winner show winning message
+	}
+}
