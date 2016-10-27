fb784689cc352995edee026c958f0fc68bcfe251
chenandy-ca <chenandy92@hotmail.com>
2016-10-26 20:00:59 -0400

    New map section, turrets are now prefabs, removed testing scene

---
+++ Dashopolis/Assets/Turret.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	public GameObject projectile;
+	public float fireRate;
+	public float delay;
+	
+	// Use this for initialization
+	void Start () {
+		//InvokeRepeating ("Shoot", delay, fireRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Instantiate(projectile,transform.position, transform.rotation);
+		//GetComponent<AudioSource>().Play();
+	}
+}
