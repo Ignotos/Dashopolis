5948783466957fe6ce6c213500d037cae6f4b818
chenandy-ca <chenandy92@hotmail.com>
2016-10-27 23:17:50 -0400

    shooting fireballs pew pew

---
+++ Dashopolis/Assets/CaveFlamePit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveFlamePit : MonoBehaviour {
+	public GameObject projectile;
+	//public Transform projectileSpawnPoint;
+	public float fireRate;
+	public float delay;
+	
+	// Use this for initialization
+	void Start () {
+		fireRate = 0.25f;
+		delay = 0.5f;
+		InvokeRepeating ("Shoot", delay, fireRate);
+	}
+	
+	// Update is called once per frame
+	void Shoot () {
+		int randomXoffset = Random.Range(-13,13);	
+		Instantiate(projectile,new Vector3(transform.position.x + randomXoffset + 0.5f, transform.position.y,transform.position.z), transform.rotation);
+	}
+}
---
+++ Dashopolis/Assets/Fireball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float lifetime = Random.Range(0.5f,2f);
+		Destroy(gameObject,lifetime);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.up * Time.deltaTime * 10);	
+	}
+}
