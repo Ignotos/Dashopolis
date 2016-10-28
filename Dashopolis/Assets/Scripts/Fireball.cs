bbd8b432a9745a98a86b578e797ab79f91758345
Ignotos <ignotomichael@gmail.com>
2016-10-28 14:30:29 -0400

    fireball speed

--- Dashopolis/Assets/Scripts/Fireball.cs
+++ Dashopolis/Assets/Scripts/Fireball.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Fireball : MonoBehaviour {
 
+    public float fireballSpeed;
+
 	// Use this for initialization
 	void Start () {
 		float lifetime = Random.Range(0.5f,2f);
@@ -10,7 +12,7 @@ public class Fireball : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector2.up * Time.deltaTime * 10);	
+		transform.Translate(Vector2.up * Time.deltaTime * fireballSpeed);	
 	}
 	
 	void OnTriggerEnter2D(Collider2D other){
