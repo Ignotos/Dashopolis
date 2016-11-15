e83a028c44f484f181cc27ad61bffccbf1c27fe2
Brendan <brendan.blencowe@gmail.com>
2016-11-14 19:13:38 -0500

    Added End Scene
    I hope this doesn’t mess anything up

--- Dashopolis/Assets/Scripts/FinishLine.cs
+++ Dashopolis/Assets/Scripts/FinishLine.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour {
 
-	
-	void OnCollisionExit2D(Collision2D col){
+	//Should Be on Enter
+	void OnCollisionEnter2D(Collision2D col){
 		//if winner show winning message
+
+		Debug.Log ("HERE");
+		if (col.gameObject.tag == "Player") {
+			PlayerController p = col.gameObject.GetComponent<PlayerController> ();
+			if (p.playerNumber == 0) {
+				PlayerPrefs.SetInt ("Player 1", 1);
+			} else
+				PlayerPrefs.SetInt ("Player 1", -1);
+			SceneManager.LoadScene ("EndScene");
+		}
 	}
 }
