535213596b14218ecb01e92bd7409a7d6a40a4ca
strg <sylvie.trg@gmail.com>
2016-11-22 19:44:42 -0500

    Added the moving saws

--- Dashopolis/Assets/Scripts/Saw.cs
+++ Dashopolis/Assets/Scripts/Saw.cs
@@ -5,10 +5,13 @@ public class Saw : MonoBehaviour {
 
     public float speed;
     public static bool timeFreezeActivated;
+    public Vector2 target;
+    private Vector2 oriPosition;
   
 	// Use this for initialization
 	void Start () {
         timeFreezeActivated = false;
+        oriPosition = new Vector2(transform.position.x, transform.position.y);
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,19 @@ public class Saw : MonoBehaviour {
         if (!timeFreezeActivated)
         {
             transform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+
+        Vector2 direction;
+        float time = Mathf.Ceil(Time.time);
+        if (time % 2 == 0)
+        {
+             direction = target - getCurrentPosition2D();
+             transform.Translate(direction * Time.deltaTime * 5.0f, Space.World);
+        }
+        else if (time % 2 != 0)
+        {
+            direction = oriPosition - getCurrentPosition2D();
+            transform.Translate(direction * Time.deltaTime * 5.0f, Space.World);
         }
     }
 
@@ -28,4 +44,10 @@ public class Saw : MonoBehaviour {
     {
         timeFreezeActivated = false;
     }
+
+    private Vector2 getCurrentPosition2D()
+    {
+        return new Vector2(transform.position.x, transform.position.y);
+    }
 }
+
