2f6831e6eb778e550b32a60ffe1aec5d03a1452f
strg <sylvie.trg@gmail.com>
2016-11-23 20:09:33 -0500

    arrows + caveplatform + saw fx

---
+++ Dashopolis/Assets/Scripts/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour
+{
+
+    public float speed;
+    public static bool timeFreezeActivated;
+   // private float lifetime;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeFreezeActivated = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!timeFreezeActivated)
+        {
+            transform.Translate(-Vector2.right * Time.deltaTime * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    public static void ActivateTimeFreeze()
+    {
+        timeFreezeActivated = true;
+    }
+
+    public static void DeactivateTimeFreeze()
+    {
+        timeFreezeActivated = false;
+    }
+}
