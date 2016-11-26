893c8d3ab743be1361d6930a8c687703ccd599f5
Brendan <brendan.blencowe@gmail.com>
2016-11-25 21:23:39 -0500

    Wild Pterodactyl Appeared
    There’s a pteradactyl

---
+++ Dashopolis/Assets/AnimalFactory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalFactory : MonoBehaviour {
+
+	[SerializeField]
+	GameObject pteraPrefab;
+	bool pteraAlive;
+
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		pteraAlive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.fixedDeltaTime;
+
+		if ((int)time % 9 == 0) {
+			if (!pteraAlive) {
+				Instantiate (pteraPrefab, pteraPrefab.transform.position, Quaternion.identity);
+				pteraAlive = true;
+			}
+		} else
+			pteraAlive = false;
+	}
+}
---
+++ Dashopolis/Assets/PteraMover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PteraMover : MonoBehaviour {
+
+	Rigidbody2D rb;
+	int yMin = -400;
+
+	public static bool timeFreezeActivated;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		timeFreezeActivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!timeFreezeActivated) {
+			//transform.Translate (Vector2.left * 5 * Time.deltaTime);
+			rb.velocity = Vector2.left * 20 ;//* Time.deltaTime;
+			Debug.Log(rb.position.x);
+			if (rb.position.x < yMin)
+				Destroy (this.gameObject);
+
+
+		}
+	}
+
+	public static void ActivateTimeFreeze()
+	{
+		timeFreezeActivated = true;
+	}
+
+	public static void DeactivateTimeFreeze()
+	{
+		timeFreezeActivated = false;
+	}
+}
