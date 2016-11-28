f744dd7b323e855966e26cdf283893f18f795922
chenandy-ca <chenandy92@hotmail.com>
2016-11-28 14:36:54 -0500

    Less ghetto starting area + other small fixes

---
+++ Dashopolis/Assets/Banana.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Banana : MonoBehaviour {
+
+		void OnCollisionEnter2D(Collision2D other){
+			if(other.gameObject.CompareTag("Player")){
+			Destroy(gameObject);	
+			}
+	 }
+}
