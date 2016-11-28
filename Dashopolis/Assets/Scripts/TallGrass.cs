9e0d4a4239067be149a4432c36e41415b3decfdd
strg <sylvie.trg@gmail.com>
2016-11-28 01:34:24 -0500

    Tall grass section fx

---
+++ Dashopolis/Assets/Scripts/TallGrass.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TallGrass : MonoBehaviour {
+
+    // Attributes
+    private Rigidbody2D rb;
+    public AudioSource audio;
+    public GameObject particles;
+   /* private bool play;
+    private Vector2 lastPos;*/
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+     /*   play = false;
+        lastPos = new Vector2(transform.position.x, transform.position.y);*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     /*   if (play && HasNewPos())
+            audio.Play();
+        else if (play && !HasNewPos())
+            audio.Pause();
+        else if (!play)
+            */
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            audio.Play();
+            Instantiate(particles, other.gameObject.transform.position, other.gameObject.transform.rotation);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            audio.Stop();
+    }
+
+  /*  Vector2 GetPos()
+    {
+        return new Vector2(transform.position.x, transform.position.y);
+    }
+
+    bool HasNewPos()
+    {
+        if (Mathf.Approximately(GetPos().x, lastPos.x) && Mathf.Approximately(GetPos().y, lastPos.y))
+            return false;
+        else
+            return true;
+    }*/
+}
---
+++ Dashopolis/Assets/Scripts/TallGrassLeaves.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TallGrassLeaves : MonoBehaviour {
+
+    // Attributes
+    private Rigidbody2D rb;
+    private Vector2 oriPosition;
+    private Vector2 targetPosition;
+    private int state; // 0: stay 1: down 2: up
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        oriPosition = new Vector2(transform.position.x, transform.position.y);
+        float rd = Random.Range(0.5f, 1.0f);
+        targetPosition = new Vector2(oriPosition.x, oriPosition.y - rd);
+        state = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckState();
+
+        // DOWN
+        if (state == 1)
+        {
+            Vector2 dir = targetPosition - GetPos();
+            transform.Translate(dir * Time.deltaTime);
+        }
+        // UP
+        else if (state == 2)
+        {
+            Vector2 dir = oriPosition - GetPos();
+            transform.Translate(dir * Time.deltaTime);
+        }
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            state = 1;
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+        else
+            state = 0;
+    }   
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            state = 2;
+        else
+            state = 0;
+    }
+
+    void CheckState()
+    {
+        if (state == 1)
+        {
+            if (Mathf.Approximately(GetPos().y, targetPosition.y))
+                state = 0;
+        }
+        else if (state == 2)
+        {
+            if (Mathf.Approximately(GetPos().y, oriPosition.y))
+                state = 0;
+        }
+    }
+
+    Vector2 GetPos()
+    {
+        return new Vector2(transform.position.x, transform.position.y);
+    }
+}
