8c8bb8f4f2fe2fe7f4c7da6cec6d0d5b840a8f1b
Brendan <brendan.blencowe@gmail.com>
2016-11-29 00:51:20 -0500

    ControlsScreen, need input from both players

---
+++ Dashopolis/Assets/ControlManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlManager : MonoBehaviour {
+
+	Text p1Ready;
+	Text p2Ready;
+	Button goBtn;
+
+	bool p1Go;
+	bool p2Go;
+
+	AudioSource selection;
+
+	// Use this for initialization
+	void Awake () {
+		Text[] ts = transform.parent.GetComponentsInChildren<Text> ();
+		p1Ready = ts [1];
+		p2Ready = ts [2];
+
+		Debug.Log (p1Ready.text);
+
+		goBtn = transform.parent.GetComponentInChildren<Button> ();
+		selection = GetComponent<AudioSource> ();
+
+		goBtn.gameObject.SetActive(false);
+		p1Ready.gameObject.SetActive (false);
+		p2Ready.gameObject.SetActive (false);
+
+		p1Go = false;
+		p2Go = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("KB_Jump")) {
+			p1Go = true;
+			selection.Play ();
+			p1Ready.gameObject.SetActive (true);
+		}
+
+		if (Input.GetButtonDown ("P1_Jump")) {
+			p2Go = true;
+			selection.Play ();
+			p2Ready.gameObject.SetActive (true);
+		}
+
+		if(p1Go && p2Go)
+			SceneManager.LoadScene("Cave_no_platform");
+	}
+}
